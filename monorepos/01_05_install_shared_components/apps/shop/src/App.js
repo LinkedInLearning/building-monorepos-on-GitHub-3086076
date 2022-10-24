23b0a31d35bf49af4c66afb7e6d7d3209d256096
Steven Moser <mellotronplayer@gmail.com>
2022-10-24 13:34:12 -0700

    Update App.js

--- monorepos/01_05_install_shared_components/apps/shop/src/App.js
+++ monorepos/01_05_install_shared_components/apps/shop/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import {BigRedButton} from '@my-libs/component';
 
 function App() {
   return (
     <div className="App">
-      <BigRedButton></BigRedButton>
+      
     </div>
   );
 }
