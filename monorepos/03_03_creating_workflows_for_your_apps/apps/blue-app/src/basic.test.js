d1e678606c634d29908a8b4b73ad994d1396576a
Carlos Solis <carlos.solis.d@gmail.com>
2025-07-22 03:54:29 -0400

    base

---
+++ monorepos/03_03_creating_workflows_for_your_apps/apps/blue-app/src/basic.test.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('basic test', () => {
+  it('should add numbers correctly', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
---
+++ monorepos/03_03_creating_workflows_for_your_apps/apps/red-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App container" style={{ height: '100vh' }}>
+        <h1 className='text-danger'>This is the Red App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
