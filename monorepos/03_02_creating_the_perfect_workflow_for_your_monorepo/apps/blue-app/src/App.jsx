3f34ac0a684849138f292c5c8265bcee361d1efe
Carlos Solis <carlos.solis.d@gmail.com>
2025-07-22 03:47:32 -0400

    base

---
+++ monorepos/03_02_creating_the_perfect_workflow_for_your_monorepo/apps/blue-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App container" style={{ height: '100vh' }}>
+        <h1 className='text-primary'>This is the Blue App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
